646b94409329f214e6169bfcfc823cadbe9a91d4
Branislav Bily <branislav.bily@gmail.com>
2018-05-25 19:00:25 +0200

    Poistka lebo mi Samo znici css

---
+++ register.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css">
+	<title>Registration system PHP and MySQL</title>
+</head>
+<body>
+<div class="header">
+	<h2>Register</h2>
+</div>
+<form method="post" action="register.php">
+	<div class="input-group">
+		<label>Username</label>
+		<input type="text" name="username" value="">
+	</div>
+	<div class="input-group">
+		<label>Email</label>
+		<input type="email" name="email" value="">
+	</div>
+	<div class="input-group">
+		<label>Password</label>
+		<input type="password" name="password_1">
+	</div>
+	<div class="input-group">
+		<label>Confirm password</label>
+		<input type="password" name="password_2">
+	</div>
+	<div class="input-group">
+		<button type="submit" class="btn" name="register_btn">Register</button>
+	</div>
+	<p>
+		Already a member? <a href="login.php">Sign in</a>
+	</p>
+</form>
+</body>
+</html>
